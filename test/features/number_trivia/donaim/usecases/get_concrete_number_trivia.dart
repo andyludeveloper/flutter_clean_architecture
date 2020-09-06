c18c819b3535898ea9f59ec6136d9c66e0531ce9
AndyLu <andyludeveloper@gmail.com>
2020-09-06 17:56:15 +0800

    feat: Write 1st unit testing for NumberTriviaRepository

---
+++ test/features/number_trivia/donaim/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+}
