0927486d36afcbd347ba00cab6735c66d9932aba
AndyLu <andyludeveloper@gmail.com>
2020-09-06 17:57:19 +0800

    feat: Create empty usecase (GetConcreteNumberTrivia) for let unit testing compile success

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
