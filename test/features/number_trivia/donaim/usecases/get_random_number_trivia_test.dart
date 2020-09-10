39e91dc09c84e91d0dafd17a08bd14406adbceea
AndyLu <andyludeveloper@gmail.com>
2020-09-10 10:24:11 +0800

    feat: Add new usecase GetRandomNumberTrivia
    1. Create GetRandomNumberTrivia class.
    2. Let get_random_number_trivia_test compile success.

--- test/features/number_trivia/donaim/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/donaim/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -19,17 +20,17 @@ void main() {
 
   test('should get trivia from the repository', () async {
     //Arrange
-    when(mockNumberTriviaRepository.getNumberNumberTrivia())
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
         .thenAnswer((_) async => Right(tNumberTrivia));
     //act
-    final result = await usecase();
+    final result = await usecase(NoParams());
 
     //assert
     expect(result, Right(tNumberTrivia));
     //Actually, we wouldn't find out because the mocked repository always returns
     //the same tNumberTrivia
     // Verification is therefore a life-saver!
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
