82f61f76bbc360a4107add3a331194e4b9d4eb4d
AndyLu <andyludeveloper@gmail.com>
2020-09-10 10:35:54 +0800

    feat: Add new usecase GetRandomNumberTrivia
    1. Move the NoParam to core/usecases/usecase.dart

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -14,8 +14,3 @@ class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
     return await repository.getRandomNumberTrivia();
   }
 }
-
-class NoParams extends Equatable {
-  @override
-  List<Object> get props => [];
-}
