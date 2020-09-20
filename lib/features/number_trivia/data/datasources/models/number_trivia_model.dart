0ba72e17f7bd3c3d22b373bff3a892a91b9e950d
AndyLu <andyludeveloper@gmail.com>
2020-09-20 15:30:31 +0800

    Fix compile-failure in NumberTriviaModel - Add fromJson method

--- lib/features/number_trivia/data/datasources/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/datasources/models/number_trivia_model.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 
 class NumberTriviaModel extends NumberTrivia {
-  final int number;
-  final String text;
-  NumberTriviaModel({this.number, this.text});
+  NumberTriviaModel({int number, String text});
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> input) {
+    final int number = input['number'];
+    final String text = input['text'];
+    return NumberTriviaModel(number: number, text: text);
+  }
 }
