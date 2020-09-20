0314d14de38d9c41a3a0908d10543479b6d8a4b1
AndyLu <andyludeveloper@gmail.com>
2020-09-20 15:18:05 +0800

    Create a unit test to verify NumberTriviaModel - should return valid model when JSON number is an integer. - In this moment, the NumberTriviaModel.fromJson is not exist, it will compile failure.

--- test/features/number_trivia/data/datasources/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/datasources/models/number_trivia_model_test.dart
@@ -1,10 +1,27 @@
+import 'dart:convert';
+
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/data/datasources/models/number_trivia_model.dart';
 import 'package:flutter_clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
   test('should be subclass of NumberTrivia entity', () async {
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group('fromJson', () {
+    test('should return valid model when JSON number is an integer', () {
+      //arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+
+      //assert
+      expect(result, tNumberTriviaModel);
+    });
   });
 }
